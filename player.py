51f73f3b8bdf26cceea7bd5bb54eba9a144c9934
Nevil Panchal <panchalnevil15@gmail.com>
2022-04-09 22:53:02 +0530

    play button added

--- player.py
+++ player.py
@@ -1,13 +1,18 @@
 from cProfile import label
 from tkinter import *
+from pygame import mixer
 
 root = Tk()
 root.geometry("600x450")
 root.title("Music Player")
 root.iconbitmap(r'img\\log1.ico')
 
-text = Label(root, text="Let's Play Music to Rock")
-text.pack()
+text = Label(root, text="Let's Play Music to Rock").pack()
 
-pic = PhotoImage(file="img\\log.png")
+def play():
+    print("Lets play music")
+
+pic = PhotoImage(file="img\\play6.png", height=100, width=100)
+Button = Button(root,image = pic, height=100, width=100, command=play).pack()
+
 root.mainloop()
